acb0454fd9b05ebe5d4184cc2ff9fde4503b3180
blackmonk13 <spencervalentino13@gmail.com>
2020-06-05 17:16:17 +0300

    Add requirements

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # get-logo
-Ascii logo generator
+Ascii logo generator.
+
+## Getting Started
+```bash
+git clone https://github.com/blackmonk13/get-logo
+cd get-logo
+pip install --user -r requirements.txt
+```
+
+Start by running the console:
+
+`python get-logo.py`
+
+### Optional:
+`echo -e 'alias get-logo="/path/to/get-logo.py"' >> .bashrc`
+
+or
+
+`echo -e 'alias get-logo="/path/to/get-logo.py"' >> .profile`
--- get-logo.py
+++ get-logo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Copyright 2020 Blackmonk13. All rights reserved.
 #
-# This software is provided under under the 
+# This software is provided under under the
 # GNU GPL V3 License. See the accompanying LICENSE file
 # for more information.
 #
@@ -14,7 +14,29 @@ import argparse
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'
-    }
+}
+
+api_url = "http://artii.herokuapp.com/make?text="
+
+
+def getlogo(logostr):
+    req = requests.get(api_url + logostr, headers=headers, timeout=10)
+
+    if req.status_code == requests.codes.ok:
+        ascii_content = req.text
+        ascii_soup = BeautifulSoup(ascii_content, features="html5lib")
+        logo_soup = ascii_soup.find('body').text
+        print(logo_soup)
+
+
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('logo', action='store', type=str)
+    options = parser.parse_args()
+    return options
+
 
 if __name__ == "__main__":
-  print("get-logo")
+    options = get_options()
+    lg = options.logo
+    getlogo(lg)
